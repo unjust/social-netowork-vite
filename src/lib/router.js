adb535e68b29d9ec05cb12c59e1c2977edb07a29
ivy <unjust@gmail.com>
2022-10-24 21:14:45 -0500

    getPath for production

--- src/lib/router.js
+++ src/lib/router.js
@@ -10,3 +10,10 @@ const routes = {
 export const navigate = (route) => {
   routes[route](document.querySelector('main'));
 };
+
+export const getPath = (path) => {
+  const publicUrlPath = /social-network-vite(.*)/;
+  const match = (publicUrlPath).exec(path);
+  // in preview mode, we are in prod build, but previewing locally so url is localhost
+  return (import.meta.env.PROD && match && match.length === 2) ? match[1] : path;
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-// Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
-import { navigate } from './lib/router.js';
+import { navigate, getPath } from './lib/router.js';
 
 myFunction();
 
-window.onload = () => navigate(`${window.location.pathname}`);
+window.onload = () => navigate(getPath(window.location.pathname));
 window.onhashchange = () => {
   const path = `/${window.location.hash.substring(1)}`;
   navigate(path);
-  window.history.pushState({}, '', `${window.location.href}${path}`);
+  window.history.replaceState({}, '', `${window.location.origin}${path}`);
 };
+window.onpopstate = () => console.log('pop state');
